a8bd9bd11e25273c8be680e1f13f175bad3b731f
Irina Khvostova <368991@niuitmo.ru>
2024-10-13 18:10:54 +0300

    optimized

--- src/components/tool-bar/tool-bar.js
+++ src/components/tool-bar/tool-bar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import styles from './tool-bar.module.css';
 import TuneIcon from '@mui/icons-material/Tune';
 import CropIcon from '@mui/icons-material/Crop';
 import Rotate90DegreesCcwIcon from '@mui/icons-material/Rotate90DegreesCcw';
@@ -7,48 +7,29 @@ import PhotoFilterIcon from '@mui/icons-material/PhotoFilter';
 import TextureIcon from '@mui/icons-material/Texture';
 import WallpaperIcon from '@mui/icons-material/Wallpaper';
 import TitleIcon from '@mui/icons-material/Title';
-import styles from './tool-bar.module.css';
+import React, { useState } from 'react';
 
 const ToolBar = () => {
   const [activeIcon, setActiveIcon] = useState(-1);
-  const handleIconClick = (index) => {
-    setActiveIcon(index);
-  };
-
+  const icons = [
+    TuneIcon,
+    CropIcon,
+    Rotate90DegreesCcwIcon,
+    SettingsOverscanIcon,
+    PhotoFilterIcon,
+    TextureIcon,
+    WallpaperIcon,
+    TitleIcon,
+  ];
   return (
     <div className={styles.mainContainer}>
-      <TuneIcon
-        className={`${styles.icon} ${activeIcon === 0 ? styles.clicked : styles.default}`}
-        onClick={() => handleIconClick(0)}
-      />
-      <CropIcon
-        className={`${styles.icon} ${activeIcon === 1 ? styles.clicked : styles.default}`}
-        onClick={() => handleIconClick(1)}
-      />
-      <Rotate90DegreesCcwIcon
-        className={`${styles.icon} ${activeIcon === 2 ? styles.clicked : styles.default}`}
-        onClick={() => handleIconClick(2)}
-      />
-      <SettingsOverscanIcon
-        className={`${styles.icon} ${activeIcon === 3 ? styles.clicked : styles.default}`}
-        onClick={() => handleIconClick(3)}
-      />
-      <PhotoFilterIcon
-        className={`${styles.icon} ${activeIcon === 4 ? styles.clicked : styles.default}`}
-        onClick={() => handleIconClick(4)}
-      />
-      <TextureIcon
-        className={`${styles.icon} ${activeIcon === 5 ? styles.clicked : styles.default}`}
-        onClick={() => handleIconClick(5)}
-      />
-      <WallpaperIcon
-        className={`${styles.icon} ${activeIcon === 6 ? styles.clicked : styles.default}`}
-        onClick={() => handleIconClick(6)}
-      />
-      <TitleIcon
-        className={`${styles.icon} ${activeIcon === 7 ? styles.clicked : styles.default}`}
-        onClick={() => handleIconClick(7)}
-      />
+      {icons.map((Icon, index) => (
+        <Icon
+          key={index}
+          className={`${styles.icon} ${activeIcon === index ? styles.clicked : styles.default}`}
+          onClick={() => setActiveIcon(index)}
+        />
+      ))}
     </div>
   );
 };
